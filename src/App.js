81f0f89251a92ae1ec306163c0a58f37ba977693
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-12-20 23:05:42 +0200

    header

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route } from 'react-router-dom';
+
+
+import Container from './components/Container';
+import Navigation from './components/Navigation';
+
+
+export default function App() {
+  
+  return (
+    <>
+    <Navigation/>
+    <Container>      
+      
+    </Container>
+    </>
+  )
+}
+
+ 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import {NavLink} from 'react-router-dom'
+import PropTypes from 'prop-types';
+
+
+import s from './Navigation.module.css';
+
+export default function Navigation() {
+  return (
+    <header>
+      <nav>
+        <NavLink to='/' className={({ isActive }) => isActive ? s.activeLink : s.link}>Home</NavLink> 
+        <NavLink to='/movies' className={({ isActive }) => isActive ? s.activeLink : s.link}>Movies</NavLink>
+      </nav>
+    </header>    
+  )
+}
